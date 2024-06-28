6ab096b206a4ce9c8620b702c1efe1a6ac593825
星 優太 <youtaixing753@gmail.com>
2024-06-28 13:44:20 +0900

    initialize

---
+++ BearHunter/BearHunter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BearHunter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BearHunter/BearHunter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BearHunter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
